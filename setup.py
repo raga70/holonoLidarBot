db5d8e8caee341bb62561ea00b77e6f35503c99b
ChrisS550 <368537@student.fontys.nl>
2024-09-19 11:43:42 +0200

    changed setup.py to wheel kinematic service

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'my_node = my_package.my_node:main'
+            'wheel_kinematics_service = my_package.wheel_kinematics_service:main'
         ],
     },
 )
