77ac8a5e7bf3c3e477840339c94611801fece855
ChrisS550 <368537@student.fontys.nl>
2024-09-23 13:11:07 +0200

    finished odomtery start should probably look at the reading out of the serial since this is probably the wrong port

--- my_package/my_package/odometry_publisher.py
+++ my_package/my_package/odometry_publisher.py
@@ -1,23 +1,28 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Quaternion, TransformStamped
 from nav_msgs.msg import Odometry
 import numpy as np
 import serial 
 import math
 from kinematics_mechanum_wheel import KinematicMechanumWheel
 from math_utils import euler_to_quaternion
+import tf2_ros
 
 class OdometryPublishing(Node):
 
     def __init__(self) -> None:
         super().__init__('odometry_publishing')
-        self.subscription = self.create_publisher(Odometry, 'turtle1/odom', 10)
+        self.odom_publisher = self.create_publisher(Odometry, '/odom', 10)
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
         y_to_wheel = (15/100)
         x_to_wheel =  (15/100)
         radius = ((8/2)/100) 
         angle_from_wheels = np.pi/2
-        timer_period = 0.5
+        timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.odom_timer_callback)
         self.wheel = KinematicMechanumWheel(y_to_wheel, x_to_wheel, radius, angle_from_wheels)
         self.serial_port = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
@@ -42,8 +47,52 @@ class OdometryPublishing(Node):
             self.y += delta_x * math.cos(self.theta) + delta_y * math.sin(self.theta)
             self.theta += delta_theta
             odom = Odometry()
+            odom.header.stamp = current_time.to_msg()
+            odom.header.frame_id = 'odom'
+            odom.child_frame_id = 'base_link'
             odom.pose.pose.position.x = self.x
             odom.pose.pose.position.y = self.y
             odom.pose.pose.position.z = 0.0
 
             orientation = euler_to_quaternion(0, 0, self.theta)
+            odom.pose.pose.orientation = Quaternion(orientation)
+
+            odom.twist.twist.linear.x = robot_velocities[0]
+            odom.twist.twist.linear.y = robot_velocities[1]
+            odom.twist.twist.angular.z = robot_velocities[2]
+            # NOTE(Chris): estimates these should be updated with actual covariances
+            odom.pose.covariance = [0.01, 0, 0, 0, 0, 0,
+                                0, 0.01, 0, 0, 0, 0,
+                                0, 0, 99999, 0, 0, 0,
+                                0, 0, 0, 99999, 0, 0,
+                                0, 0, 0, 0, 99999, 0,
+                                0, 0, 0, 0, 0, 0.1]  
+
+            odom.twist.covariance = [0.01, 0, 0, 0, 0, 0,
+                                 0, 0.01, 0, 0, 0, 0,
+                                 0, 0, 99999, 0, 0, 0,
+                                 0, 0, 0, 99999, 0, 0,
+                                 0, 0, 0, 0, 99999, 0,
+                                 0, 0, 0, 0, 0, 0.1]  
+
+            self.odom_publisher.publish(odom)
+            self.publish_tf(current_time)
+            self.last_time - current_time
+
+        def publish_tf(self, current_time):
+            t = TransformStamped()
+            t.header.stamp = current_time.to_msg()
+            t.header.frame_id = 'odom'
+            t.child_frame_id = 'base_link'
+
+            t.transform.translation.x = self.x
+            t.transform.translation.y = self.y
+            t.transform.translation.z = 0.0
+
+            orientation =  euler_to_quaternion(0, 0, self.theta)
+            t.transform.rotation.x = orientation[0] 
+            t.transform.rotation.y = orientation[1] 
+            t.transform.rotation.z = orientation[2] 
+            t.transform.rotation.w = orientation[3] 
+
+            self.tf_broadcaster.sendTransform(t)
