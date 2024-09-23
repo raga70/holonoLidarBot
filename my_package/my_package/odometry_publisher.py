4d75f48924d379ec4bd01a64aed9eb54e9c3ab7f
ChrisS550 <368537@student.fontys.nl>
2024-09-23 14:19:54 +0200

    added functions to calculate the variance created due to gearbox backlash and the resolution of the encoders

--- my_package/my_package/odometry_publisher.py
+++ my_package/my_package/odometry_publisher.py
@@ -9,6 +9,34 @@ from kinematics_mechanum_wheel import KinematicMechanumWheel
 from math_utils import euler_to_quaternion
 import tf2_ros
 
+def var_resolution(wheel_radius, resolution):
+    """ Calculates the variance created by the encoder resolution.
+        This is in meters
+
+    Args:
+        wheel_radius (_type_): wheel radius in meters 
+        resolution (_type_): Resolution of the encoder in ticks per revolution 
+    """
+    delta = (2*math.pi*wheel_radius) / resolution
+
+    return (delta/2)**2
+
+def var_gearbox_backlash(encoder_ang_vel, wheelbacklash, wheel):
+    """ Calculates the effect of the backlash on the velocities.
+        This is done by using the kinematics equation, so that we have
+        an accurate estimate of the contribution of the slippage of every wheel.
+        We only use the sign of the encoder angular velocities, and the magnitude of 
+        the wheelbacklash
+
+    Args:
+        encoder_ang_vel (_type_): An array with the angular velocities givenback by the encoders
+        wheelbacklash (_type_): the wheel backlash in radians 
+        wheel (_type_): Wheel object, used for the kinematics 
+    """
+
+    wheel_slippages = np.sign(encoder_ang_vel) * wheelbacklash
+    return wheel.calculate_robot_velocities(wheel_slippages)
+
 class OdometryPublishing(Node):
 
     def __init__(self) -> None:
@@ -62,7 +90,7 @@ class OdometryPublishing(Node):
             odom.twist.twist.angular.z = robot_velocities[2]
             # NOTE(Chris): estimates these should be updated with actual covariances
             odom.pose.covariance = [0.01, 0, 0, 0, 0, 0,
-                                0, 0.01, 0, 0, 0, 0,
+                                    0, 0.01, 0, 0, 0, 0,
                                 0, 0, 99999, 0, 0, 0,
                                 0, 0, 0, 99999, 0, 0,
                                 0, 0, 0, 0, 99999, 0,
