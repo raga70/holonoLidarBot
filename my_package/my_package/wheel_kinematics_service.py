314dd87f84a116d6fdc475502b14c3133ee57d5e
ChrisS550 <368537@student.fontys.nl>
2024-09-24 10:27:40 +0200

    added angular velocity scaling to scale to in between -11 and 11

--- my_package/my_package/wheel_kinematics_service.py
+++ my_package/my_package/wheel_kinematics_service.py
@@ -14,6 +14,8 @@ class KinematicsProcessing(Node):
         y_to_wheel = (15/100)
         x_to_wheel =  (15/100)
         radius = ((8/2)/100) 
+        self.max_angular_velocities = 60
+        self.max_output_angular_velocities = 11
         angle_from_wheels = np.pi/2
         self.wheel = KinematicMechanumWheel(y_to_wheel, x_to_wheel, radius, angle_from_wheels)
         self.serial = serial.Serial("/dev/ttyAMA0", 9600)
@@ -24,6 +26,7 @@ class KinematicsProcessing(Node):
             [msg.linear.x, msg.linear.y, msg.angular.z]
             )
         wheel_ang_velocities = self.wheel.calculate_wheel_velocities(velocities)
+        wheel_ang_velocities = wheel_ang_velocities / (self.max_angular_velocities/self.max_output_angular_velocities)
         self.get_logger().info(f"{wheel_ang_velocities}")
         # 1st motor front left first value 
         # 2nd motor front right second
