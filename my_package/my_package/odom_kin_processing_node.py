17f7caa8efb73f99b872420ba9500239bc70b55c
ChrisS550 <368537@student.fontys.nl>
2024-10-03 14:49:05 +0200

    f

--- my_package/my_package/odom_kin_processing_node.py
+++ my_package/my_package/odom_kin_processing_node.py
@@ -122,10 +122,10 @@ class KinOdomProcessing(Node):
         # Optional quadrant-based sign adjustments
         print(f'theta: {np.rad2deg(self.theta)}')
         if np.pi / 2 <= self.theta < np.pi:  # Second quadrant (90 to 180 degrees)
-            delta_x = -delta_x
-            # tmp = delta_x
-            # delta_x = -delta_y  # Cos is negative in second quadrant
-            # delta_y = tmp 
+            #delta_x = -delta_x
+            tmp = delta_x
+            delta_x = delta_y  # Cos is negative in second quadrant
+            delta_y = tmp 
         elif np.pi <= self.theta < 3*np.pi / 2:  # Third quadrant (180 to 270 degrees)
             print("DEBUG")
             delta_x = delta_x  # Cos is negative
