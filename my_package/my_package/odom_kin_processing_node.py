7af1a9c62f48a0b75099d5645872d7e096d55a49
ChrisS550 <368537@student.fontys.nl>
2024-10-03 13:57:59 +0200

    dfjl

--- my_package/my_package/odom_kin_processing_node.py
+++ my_package/my_package/odom_kin_processing_node.py
@@ -122,6 +122,8 @@ class KinOdomProcessing(Node):
         delta_y = (vx * math.cos(self.theta) - vy * math.sin(self.theta)) * delta_time
         delta_x = (vx * math.sin(self.theta) + vy * math.cos(self.theta)) * delta_time
         # Optional quadrant-based sign adjustments
+        if 0 <= self.theta < np.pi / 2:  # First quadrant (0 to 90 degrees)
+            delta_x = -delta_x
         if np.pi / 2 <= self.theta < np.pi:  # Second quadrant (90 to 180 degrees)
             delta_x = -delta_x  # Cos is negative in second quadrant
         elif -np.pi <= self.theta < -np.pi / 2:  # Third quadrant (180 to 270 degrees)
