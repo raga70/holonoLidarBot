7ce1fc82d06438f62d889799aad14c3220df57a9
ChrisS550 <368537@student.fontys.nl>
2024-10-03 13:54:46 +0200

    s

--- my_package/my_package/odom_kin_processing_node.py
+++ my_package/my_package/odom_kin_processing_node.py
@@ -119,8 +119,8 @@ class KinOdomProcessing(Node):
         vx = robot_velocities[0]
         vy = robot_velocities[1]
         vtheta = robot_velocities[2]
-        delta_x = (vx * math.cos(self.theta) - vy * math.sin(self.theta)) * delta_time
-        delta_y = (vx * math.sin(self.theta) + vy * math.cos(self.theta)) * delta_time
+        delta_y = (vx * math.cos(self.theta) - vy * math.sin(self.theta)) * delta_time
+        delta_x = (vx * math.sin(self.theta) + vy * math.cos(self.theta)) * delta_time
         # Optional quadrant-based sign adjustments
         if np.pi / 2 <= self.theta < np.pi:  # Second quadrant (90 to 180 degrees)
             delta_x = -delta_x  # Cos is negative in second quadrant
