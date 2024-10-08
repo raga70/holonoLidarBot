6d43580d9d83fa33a35830a9876e181dfd10c1a6
Gary0915a <mrgergely2002@gmail.com>
2024-10-08 14:28:37 +0200

    modified:   StartAllHeadlessPI_laptopDependant.sh

--- StartAllHeadlessPI_laptopDependant.sh
+++ StartAllHeadlessPI_laptopDependant.sh
@@ -15,12 +15,6 @@ screen -dmS static_transform bash -c "source /home/ubuntu/ros2_ws/install/setup.
 screen -dmS static_transform bash -c "source /home/ubuntu/ros2_ws/install/setup.bash && source /opt/ros/humble/setup.bash && ros2 run tf2_ros static_transform_publisher 0.0 0 0 3.1416 0 0 base_link laser"
 
 
-# Launch SLAM Toolbox
-ros2 run slam_toolbox async_slam_toolbox_node --ros-args --param use_sim_time:=false --param map_topic:=/map --param base_frame:=base_link --param odom_frame:=odom --param scan_topic:=/scan --param minimum_travel_distance:=0.1 &
-
-# Launch Nav2
-ros2 launch nav2_bringup online_async_launch.py params_file:=src/nav2_params.yaml &
-
 
 echo "Processes started in separate screen sessions:"
 echo "  - wheel_kinematics (screen session: wheel_kinematics_odom)"
